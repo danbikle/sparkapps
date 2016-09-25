44450addc8218416f4a522661cd2aea1adae2f5a
Dan Bikle <bikle@bikle.com>
2016-09-25 02:26:28 -0700

    more

--- linr10/linr10.scala
+++ linr10/linr10.scala
@@ -17,8 +17,23 @@ import org.apache.spark.sql.SQLContext
 
 val sqlContext = new SQLContext(sc)
 
-val fb_df = sqlContext.read.format("com.databricks.spark.csv").option("header","true").option("inferSchema","true").load("/tmp/gspc.csv")
+val gspc10_df = sqlContext.read.format("com.databricks.spark.csv").option("header","true").option("inferSchema","true").load("/tmp/gspc.csv")
 
-fb_df.head(9)
+// I should Register the DataFrame as a SQL temporary view
+gspc10_df.createOrReplaceTempView("gspc10_table")
 
+var sql_str = "SELECT Date, Close, LEAD(Close,1)OVER(ORDER BY Date) AS leadp "
+    sql_str=sql_str++" FROM gspc10_table ORDER BY Date"
 
+val gspc11_df = spark.sql(sql_str)
+
+//"SELECT Date,Close, AVG(Close)OVER(ORDER BY Date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS mvgavg3day FROM gspc2_table ORDER BY Date")
+
+gspc11_df.createOrReplaceTempView("gspc11_table")
+
+var sql_str = "SELECT Date, Close, 100.0*(leadp - Close)/Close AS pctlead "
+    sql_str=sql_str++" FROM gspc11_table ORDER BY Date"
+
+val gspc12_df = spark.sql(sql_str)
+
+gspc12_df.head(5)
