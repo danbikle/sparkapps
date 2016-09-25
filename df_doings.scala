f9f22b402576039fc29eb643c08d1bebf87c5a18
Dan Bikle <bikle@bikle.com>
2016-09-25 16:42:51 -0700

    more

--- df_doings.scala
+++ df_doings.scala
@@ -17,3 +17,28 @@ case class Person(name: String, age: Int)
 
 val df = Seq((Person("john", 33), 5), (Person("mike", 30), 6)).toDF("person", "id")
 df.show()
+
+// I should get prices:
+import sys.process._
+"/usr/bin/curl -o /tmp/gspc.csv http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC"!
+
+val sqlContext = new SQLContext(sc)
+
+val gspc10_df = sqlContext.read.format("com.databricks.spark.csv").option("header","true").option("inferSchema","true").load("/tmp/gspc.csv")
+
+// Print the schema in a tree format
+gspc10_df.printSchema()
+
+// filter demo:
+gspc10_df.filter($"Close" > 2160.00).show()
+gspc10_df.filter($"Date" >= "2016-09-22").show()
+
+// collect()
+gspc10_df.filter($"Date" > "2016-09-22").collect().foreach { println _}
+
+// map()
+gspc10_df.filter($"Date" > "2016-09-22").collect().map{ row => row }
+gspc10_df.filter($"Date" > "2016-09-22").collect().map{ row => row(0) }
+gspc10_df.filter($"Date" > "2016-09-22").collect().map{ row => (row(0),row(1)) }
+gspc10_df.filter($"Date" > "2016-09-22").collect().map{ row => Array(row(0),row(1)) }
+
