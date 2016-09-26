a118b05e7359665b8a38cf42046903ea595b20a4
Dan Bikle <bikle@bikle.com>
2016-09-25 22:14:14 -0700

    more

--- logr10/logr10.scala
+++ logr10/logr10.scala
@@ -64,8 +64,11 @@ sql_str=sql_str++",(mavg3-LAG(mavg9,1)OVER(ORDER BY Date))/mavg3 AS slp9 "
 sql_str=sql_str++" FROM gspc13_table ORDER BY Date"
 val gspc14_df = spark.sql(sql_str)
 
-// I should get test data:
+// I should compute label from pctlead:
 gspc14_df.createOrReplaceTempView("gspc14_table")
-var sql_str = "SELECT Date, Close, pctlead,slp2,slp3,slp4,slp5,slp6,slp7,slp8,slp9              "
-sql_str=sql_str++"FROM gspc14_table WHERE Date BETWEEN '2016-01-01'AND'2016-01-01' ORDER BY Date"
+var sql_str = "SELECT Date,Close,pctlead,slp2,slp3,slp4,slp5,slp6,slp7,slp8,slp9"
+sql_str=sql_str++" FROM gspc14_table ORDER BY Date                              "
 val gspc15_df = spark.sql(sql_str)
+
+val gspc17_df = gspc15_df.withColumn("label",col("pctlead"))
+gspc17_df.show
